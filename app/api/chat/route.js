8069e53ef441e95f48cb10b29a2bdf6d09bc6178
Xiang Qing Wang <15shanon@gmail.com>
2024-08-08 18:33:20 -0400

    Add files via upload

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -19,7 +19,7 @@ export async function POST(req){
         messages: [
             {
                 role: "system",
-                content: SystemPrompt,
+                content: systemPrompt,
             },
             ...data,
         ],
